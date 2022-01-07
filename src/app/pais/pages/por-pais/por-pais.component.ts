dc72ed23f97ef2a2d273ae541885e11c7e1213ea
hgutierrez <hgutierrez@OSC0100239>
2022-01-07 15:22:33 -0400

    consumo de api porPais e interfaz para tipado

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Countries, ErrorPais } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,16 +9,30 @@ import { Component } from '@angular/core';
   ]
 })
 export class PorPaisComponent  {
-termino: string = ''
+termino: string = '';
+hayError: boolean = false;
+paises: Countries[] = [];
+
+
+constructor( private paisService : PaisService) { }
 
 buscar(){
+  this.hayError = false;
   if(this.termino.length === 0){return}
 
-  console.log(this.termino)
+this.paisService.buscarPais(this.termino)
+.subscribe((resp) => {let error = resp as ErrorPais;
+console.log(resp)
+
+if(error.status == '404'){
+  this.hayError = true;
+  this.paises = [];
+  return;
 }
 
-  constructor() { }
+this.paises = <Countries[]>resp;
+});
 
-
+}
 
 }
